fdcd3b1fc608d371c08460a2b9ab7e5fbb67767a
Robey Pointer <robey@lag.net>
2008-01-29 23:44:00 -0800

    [project @ robey@lag.net-20080130074400-uqrmv3ufg4fkxdp4] oh yeah, and more tests.

--- test/net/lag/configgy/AttributesTests.scala
+++ test/net/lag/configgy/AttributesTests.scala
@@ -1,5 +1,7 @@
 package net.lag.configgy
 
+import scala.util.Sorting
+
 import sorg.testing._
 
 
@@ -14,6 +16,8 @@ object AttributesTests extends Tests {
         expect("{root: name=\"Communist\" }") { s.toString }
         s.set("age", 8)
         expect("{root: age=\"8\" name=\"Communist\" }") { s.toString }
+        s.set("age", 19)
+        expect("{root: age=\"19\" name=\"Communist\" }") { s.toString }
     }
     
     test("read") {
@@ -23,8 +27,15 @@ object AttributesTests extends Tests {
         expect("Communist") { s.get("name", "") }
         expect(8) { s.getInt("age", 999) }
         expect(500) { s.getInt("unknown", 500) }
-        expect("Communist") { s("name") }
-        expect("8") { s("age") }
+        expect("Communist") {
+            s("name") match {
+                case Some(x) => x
+                case None => null
+            }
+        }
+        expect("8") { s("age", null) }
+        expect("8") { s("age", "500") }
+        expect("500") { s("unknown", "500") }
     }
     
     test("compound") {
@@ -46,5 +57,47 @@ object AttributesTests extends Tests {
         expect(None) { s.get("a.d.x") }
         // shouldn't have changed the attr map:
         expect("{: a={a: b={a.b: c=\"8\" } } }") { s.toString }
+    }
+    
+    test("equals") {
+        val s = new Attributes(null, "root")
+        s("name") = "Communist"
+        s("age") = 8
+        s("diet.food.dry") = "Meow Mix"
+        val t = new Attributes(null, "root")
+        t("name") = "Communist"
+        t("age") = 8
+        t("diet.food.dry") = "Meow Mix"
+        
+        expect(true) { s == t }
+    }
+    
+    test("remove") {
+        val s = new Attributes(null, "")
+        s("name") = "Communist"
+        s("age") = 8
+        s("diet.food") = "Meow Mix"
+        s("diet.liquid") = "water"
+        expect("{: age=\"8\" diet={diet: food=\"Meow Mix\" liquid=\"water\" } name=\"Communist\" }") { s.toString }
+        expect(true) { s.remove("diet.food") }
+        expect(false) { s.remove("diet.food") }
+        expect("{: age=\"8\" diet={diet: liquid=\"water\" } name=\"Communist\" }") { s.toString }
+    }
+
+    test("as map") {
+        val s = new Attributes(null, "")
+        s("name") = "Communist"
+        s("age") = 8
+        s("disposition") = "fighter"
+        s("diet.food") = "Meow Mix"
+        s("diet.liquid") = "water"
+        val map = s.asMap
+
+        // turn it into a sorted list, so we get a deterministic answer
+        val keyList = map.keys.toList.toArray
+        Sorting.quickSort(keyList)
+        expect("{ age=8, diet.food=Meow Mix, diet.liquid=water, disposition=fighter, name=Communist }") {
+            (for (val k <- keyList) yield (k + "=" + map(k))).mkString("{ ", ", ", " }")
+        }
     }
 }
